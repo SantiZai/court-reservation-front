f6b1b44269427f88a9e54a0f5ace482b988acf3f
SantiZai <santyagozaidan@gmail.com>
2023-08-31 22:53:18 -0300

    update

--- src/services/checking.ts
+++ src/services/checking.ts
@@ -1,6 +1,6 @@
 import { Court } from "@/utils/models";
 
-//TODO: Complete this function
+//TODO: Complete this function for check if the court have disponibility
 export const checkDisponibility = (courts: Court[]) => {
 
 }
