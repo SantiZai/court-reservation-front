0040d782dfbad024378d9ddef73bd2a0a5fe0733
SantiZai <santyagozaidan@gmail.com>
2023-09-03 13:43:54 -0300

    dividing the functionalities

--- src/services/checking.ts
+++ src/services/checking.ts
@@ -14,3 +14,62 @@ export const checkReservations = (courts: Court[]) => {
 	});
 	return reservations;
 };
+
+export const calculatePriorLimit = (
+	hour: string,
+	hoursBefore: number,
+): string => {
+	// Dividir hora
+	const [h, m] = hour.split(":");
+
+	// Convertir a números
+	let newH = parseInt(h);
+	let newM = parseInt(m);
+
+	// Restar horas
+	newH -= Math.floor(hoursBefore);
+
+	// Restar minutos
+	newM -= Math.round((hoursBefore % 1) * 60);
+
+	// Corregir minutos negativos
+	if (newM < 0) {
+		newM += 60;
+		newH--;
+	}
+
+	// Formatear con 02 dígitos
+	const newHStr = newH.toString().padStart(2, "0");
+	const newMStr = newM.toString().padStart(2, "0");
+
+	return `${newHStr}:${newMStr}`;
+};
+
+export const calculatePostLimit = (
+	hour: string,
+	hoursAfter: number,
+): string => {
+	const [h, m] = hour.split(":");
+
+	let newH = parseInt(h);
+	let newM = parseInt(m);
+
+	newH += Math.floor(hoursAfter);
+	newM += Math.round((hoursAfter % 1) * 60);
+
+	if (newM >= 60) {
+		newM -= 60;
+		newH++;
+	}
+
+	const newHStr = newH.toString().padStart(2, "0");
+	const newMStr = newM.toString().padStart(2, "0");
+
+	return `${newHStr}:${newMStr}`;
+};
+
+export const between = (arr: string[], start: string, end: string) => {
+	return arr.filter((hour) => {
+		return hour >= start && hour <= end;
+	});
+};
