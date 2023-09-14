385106ccd6b06fc91a05efaa9e303c4346ef712d
SantiZai <santyagozaidan@gmail.com>
2023-09-13 22:57:59 -0300

    todo

--- src/app/reservations/new-reservation/page.tsx
+++ src/app/reservations/new-reservation/page.tsx
@@ -19,6 +19,7 @@ const NewReservationPage = () => {
 	const newReservation = () => {
 		const actualDate = new Date();
 		try {
+			//TODO: no me permite crear reservas
 			createReservation({
 				duration: duration,
 				reservedMonth: (actualDate.getMonth() + 1).toString().padStart(2, "0"), 
