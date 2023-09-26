6717a747794b4a6c84b61539532bc876142ee69b
SantiZai <santyagozaidan@gmail.com>
2023-09-26 13:53:08 -0300

    home page in process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,58 @@
+import "./home.scss";
+
 const HomePage = () => {
 	return (
-		<div className="p-2">
-			<div className="">
-				<h1>Logo</h1>
+		<div className="container-all p-2">
+			<div>
+				<span>Logo</span>
+			</div>
+
+			<div className="flex flex-col">
+				<span>Reserva tu cancha al instante</span>
+				<span>
+					Selecciná entre las opciones disponibles en tu ciudad en tiempo real!
+				</span>
+			</div>
+
+			<div className="container-search flex flex-col p-2 rounded-2xl">
+				<input type="text" placeholder="Buscar ciudad" />
+				<input type="text" placeholder="Deporte" />
+				<div className="flex gap-2">
+					<input type="text" placeholder="Día" />
+					<input type="text" placeholder="Horario" />
+				</div>
+				<button className="btn-primary rounded-2xl">Buscar cancha</button>
+			</div>
+
+			<div className="test-service">
+				<span>Gestioná el uso de tus canchas de forma cómoda y práctica</span>
 			</div>
 			<div>
-				<h2>Reserva tu cancha al instante</h2>
-				<h4>Selecciná entre las opciones disponibles en tu ciudad en tiempo real!</h4>
+				<div className="flex flex-col">
+					<span>
+						¿Cómo puedo probar el servicio para mi institución deportiva?
+					</span>
+					<span>
+						1. Ver planes y funciones según tus necesidades y las de tus
+						clientes
+					</span>
+					<span>2. Contactarte con nosotros</span>
+					<span>
+						3. Recibes tu prueba <strong>gratis</strong> de hasta 15 días para
+						probar nuestro software
+					</span>
+				</div>
+				<div className="flex w-full justify-between">
+					<button className="btn-primary rounded-md px-3 py-1 text-sm">FUNCIONALIDADES</button>
+					<button className="btn-secondary rounded-md px-3 py-1 text-sm">PLANES Y PRECIOS</button>
+				</div>
+			</div>
+
+			<div className="w-full flex flex-col items-center">
+				<div>
+					<span>Preguntas frecuentes</span>
+				</div>
+				<div></div>
 			</div>
 		</div>
 	);
---
+++ src/components/pures/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div className="flex flex-col items-center">
+            <span>Logo</span>
+            <span>Contactanos</span>
+            <span>Redes sociales</span>
+            <span>Nuestras políticas de privacidad</span>
+        </div>
+    )
+}
+
+export default Footer
