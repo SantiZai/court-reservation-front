bf791233a11995f78c95b137a8b8785fe5d03bdb
SantiZai <santyagozaidan@gmail.com>
2023-10-10 20:23:24 -0300

    env

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/profile", "/reservations", "/new-club"] }
+export const config = { matcher: ["/profile", "/reservations", "/new-club"] };
